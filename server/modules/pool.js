690061806562ac7bdf2e1fee6163b25353fd110a
Dave Nash <davidjnash2@gmail.com>
2023-05-22 16:01:31 -0500

    update poo.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,22 @@
 const pg = require('pg');
 
-const pool = new pg.Pool({
-  database: 'weekend-to-do-app', // named current database, rest here is boilerplate
-  host: 'localhost',
-  port: 5432
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+          rejectUnauthorized: false
+      }
+  });
+}
+
+else {
+  pool = new pg.Pool({
+    database: 'weekend-to-do-app', // named current database, rest here is boilerplate
+    host: 'localhost',
+    port: 5432
 });
+}
 
 module.exports = pool;
